e0ab492039a123d346181e99d47794f801991f06
Bentley Davis <Bentley@BentleyDavis.com>
2014-09-24 18:56:28 -0500

    Sample code to take a screenshot of all screens
    This is sample code from March of 2010 form my blog. You can see the
    post at
    http://bentleydavis.com/simple-screen-shot-of-all-multiple-monitors-2/

---
+++ SaveScreen/Helpers.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Drawing.Imaging;
+using System.Windows;
+using System.Drawing;
+
+namespace Helpers
+{
+    class ScreenHelper
+    {
+        public static void SaveAllScreens( string fileName)
+        {
+            int screenWidth = Convert.ToInt32(SystemParameters.VirtualScreenWidth);
+            int screenHeight = Convert.ToInt32(SystemParameters.VirtualScreenHeight);
+            int screenLeft = Convert.ToInt32(SystemParameters.VirtualScreenLeft);
+            int screenTop = Convert.ToInt32(SystemParameters.VirtualScreenTop);
+            using (Bitmap bmp = new Bitmap(screenWidth, screenHeight, PixelFormat.Format32bppArgb))
+            {
+                using (Graphics gr = Graphics.FromImage(bmp))
+                { 
+                    gr.CopyFromScreen(screenLeft, screenTop, 0, 0, bmp.Size);
+                    bmp.Save(fileName);
+                }
+            }
+        }
+    }
+}
